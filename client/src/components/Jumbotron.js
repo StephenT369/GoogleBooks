1c2288a3e725ccf9cd45a754b921a85de56810fb
Stephen Thompson <smt3@hotmail.com>
2019-11-21 01:32:16 -0600

    added bootstrap

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../assets/css/style.css";
+
+function Jumbotron({ children }) {
+  return <div className="jumbotron mt-3">{children}</div>;
+}
+export default Jumbotron;
