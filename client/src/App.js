125411f614d92b8eea21b901e033c00e87c7779e
Stephen Thompson <smt3@hotmail.com>
2019-11-21 00:57:48 -0600

    moved some files around

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import Saved from "./pages/Saved";
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -62,7 +62,7 @@ class Main extends Component {
           <Col size="md-12">
         
               <h1 className="text-center">
-                <strong>(React) Google Books Search</strong>
+                <strong>Google Books Search</strong>
               </h1>
               <h2 className="text-center">Search for and Save Books of Interest.</h2>
 
